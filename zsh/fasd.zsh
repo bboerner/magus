93984c69532cd97591d4d3cc9bba430388103ec1
Brendan Boerner <bboerner.foss@gmail.com>
2014-06-28 23:36:38 -0500

    Don't use fasd if not installed

--- zsh/fasd.zsh
+++ zsh/fasd.zsh
@@ -1,15 +1,18 @@
 #
 # only init if installed.
-fasd_cache="$HOME/.fasd-init-bash"
-if [ "$(command -v fasd)" -nt "$fasd_cache" -o ! -s "$fasd_cache" ]; then
-  eval "$(fasd --init posix-alias zsh-hook zsh-ccomp zsh-ccomp-install zsh-wcomp zsh-wcomp-install)" >| "$fasd_cache"
-fi
-source "$fasd_cache"
-unset fasd_cache
+if $(fasd &>/dev/null)
+then
+    fasd_cache="$HOME/.fasd-init-zsh"
+    if [ "$(command -v fasd)" -nt "$fasd_cache" -o ! -s "$fasd_cache" ]; then
+      eval "$(fasd --init posix-alias zsh-hook zsh-ccomp zsh-ccomp-install zsh-wcomp zsh-wcomp-install)" >| "$fasd_cache"
+    fi
+    source "$fasd_cache"
+    unset fasd_cache
 
-alias a='fasd -a' # any
-alias s='fasd -si' # show / search / select
-alias d='fasd -d' # directory
-alias f='fasd -f' # file
-alias z='fasd_cd -d' # cd, same functionality as j in autojump
-alias t='fasd_cd -d -i' # interactive directory jump
+    alias a='fasd -a' # any
+    alias s='fasd -si' # show / search / select
+    alias d='fasd -d' # directory
+    alias f='fasd -f' # file
+    alias z='fasd_cd -d' # cd, same functionality as j in autojump
+    alias t='fasd_cd -d -i' # interactive directory jump
+fi
